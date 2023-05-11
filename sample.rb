cf0665652348d72b4833beb63fd24380eec2fde7
Yamadalinux4810 <080100704.33runner@gmail.com>
2023-05-11 20:37:09 +0900

    first commit

---
+++ sample.rb
@@ -0,0 +1,23 @@
+while true
+  puts "1~5で評価を入力してください。終了する場合は「6」を入力してください"
+  point = gets.to_i
+
+  if point == 6
+    puts "終了します"
+    break
+  elsif point != 0
+    puts "コメントを入力してください"
+    comment = gets
+    puts "ポイント：#{point}　コメント：#{comment}"
+    post = {point: point, comment: comment}
+    file = File.open("data.txt","a")
+    file.puts(post)
+    file.close
+    
+    read_file = File.open("data.txt", "r")
+    puts read_file.read
+    read_file.close
+  else
+    puts "数字を入力してください"
+  end
+end
